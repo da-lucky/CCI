aa478c5fba6696145d05f61a178a628330feae2b
da-lucky <da.ru.stp@gmail.com>
2018-04-17 16:45:06 +0300

    small updates + balancing chack added

--- CCI/tree/t.cpp
+++ CCI/tree/t.cpp
@@ -5,6 +5,7 @@
 #include <stack>
 #include <queue>
 #include <memory>
+#include <cstdlib>
 
 template<typename T>
 struct treeNode {
@@ -42,8 +43,9 @@ void createTree(std::vector<std::unique_ptr<treeNode<char>>>& v) {
 	/*C*/v[2]->right = v[12].get();/*M*/
 }
 
+//--------------------- Post order traverse -----------------------------
 template <typename U, typename F>
-void traverseTreeS(treeNode<U>* nodePtr, F nodeProc) {		
+void traverseTreePostO(treeNode<U>* nodePtr, F nodeProc) {		
 	
 	std::stack<treeNode<U>*> node_s {};
 	std::size_t max = 0, min = 0, cnt = 0;
@@ -65,28 +67,30 @@ void traverseTreeS(treeNode<U>* nodePtr, F nodeProc) {
 }
 
 template <typename U, typename F>
-void traverseTreeSRec(treeNode<U>* nodePtr, F nodeProc) {
+void traverseTreePostORec(treeNode<U>* nodePtr, F nodeProc) {
 	if(nodePtr) {
-		traverseTreeSRec(nodePtr->left, nodeProc);
-		traverseTreeSRec(nodePtr->right, nodeProc);
-		nodeProc(nodePtr);
+		traverseTreePostORec(nodePtr->left, nodeProc);
+		nodeProc(nodePtr->data);
+		traverseTreePostORec(nodePtr->right, nodeProc);
+		
 	}
 }
 
+//--------------------- Pre order traverse -----------------------------
 template <typename U, typename F>
-void _traverseTreeQ(treeNode<U>* nodePtr, F nodeProc) {		
+void _traverseTreePreO(treeNode<U>* nodePtr, F nodeProc) {		
 	
-	std::stack<treeNode<U>*> node_s {};
+	std::stack<treeNode<U>*> node_s {};	
 		
 	while (true) {
 		if(nodePtr) {
 			node_s.push(nodePtr);
 			nodeProc(node_s.top()->data);
-			nodePtr = nodePtr->left;			
+			nodePtr = nodePtr->left;
 		} else {
 			if(node_s.empty()) {
 				return;
-			}			
+			}
 			nodePtr = node_s.top()->right;
 			node_s.pop();
 		}
@@ -94,46 +98,75 @@ void _traverseTreeQ(treeNode<U>* nodePtr, F nodeProc) {
 }
 
 template <typename U, typename F>
-void traverseTreeQ(treeNode<U>* nodePtr, F nodeProc) {
+void traverseTreePreO(treeNode<U>* nodePtr, F nodeProc) {
 	if(nodePtr) {
 		nodeProc(nodePtr->data);
-		_traverseTreeQ(nodePtr->left, nodeProc);
-		_traverseTreeQ(nodePtr->right, nodeProc);
+		_traverseTreePreO(nodePtr->left, nodeProc);
+		_traverseTreePreO(nodePtr->right, nodeProc);
 		
 	}
 }
 
 template <typename U, typename F>
-void traverseTreeQRec(treeNode<U>* nodePtr, F nodeProc) {
-	if(nodePtr) {
+void traverseTreePreORec(treeNode<U>* nodePtr, F nodeProc) {
+	
+	if(nodePtr) {		
 		nodeProc(nodePtr->data);
-		traverseTreeQRec(nodePtr->left, nodeProc);
-		traverseTreeQRec(nodePtr->right, nodeProc);
+		traverseTreePreORec(nodePtr->left, nodeProc);
+		traverseTreePreORec(nodePtr->right, nodeProc);
+	}
+}
+
+//----------------- check for tree balancing -----------------------
+template <typename U>
+bool tBalaced(treeNode<U>* nodePtr) {
+	bool status = true;
+	
+	treeBalanceCheck(nodePtr, status);
+	
+	return status;
+}
+
+template <typename U>
+int treeBalanceCheck(treeNode<U>* nodePtr, bool& status) {
+	if(nodePtr) {	
+		auto l = treeBalanceCheck(nodePtr->left, status) + 1;
+		auto r = treeBalanceCheck(nodePtr->right, status) + 1;
+			
+		if(std::abs(l - r) > 1) { 
+			std::cout << "\ntree is not balanced:node " << nodePtr->data << " lh:rh " << (l - 1) << ":" << (r - 1) << " \n";		
+			status = false;
+		}
 		
+		return (l > r) ? l : r;
+	} else {
+		return 0;
 	}
 }
 
 int main() {
 
-	std::cout << "tree traverse\n";
 	std::vector<std::unique_ptr<treeNode<char>>> v;
 	createTree(v);
 	treeNode<char>* node = v.front().get();
 	
-	std::cout << "Stack \n";
-	traverseTreeS(node, [](char n) { std::cout << n << " " ;});
+	std::cout << "Post Order \n";
+	traverseTreePostO(node, [](char n) { std::cout << n << " " ;});
 	std::cout << "\n";
 	
-	std::cout << "Stack recurse\n";
-	traverseTreeS(node, [](char n) { std::cout << n << " " ;});
+	std::cout << "Post Order recurse\n";
+	traverseTreePostORec(node, [](char n) { std::cout << n << " " ;});
 	std::cout << "\n";
 	
-	std::cout << "queue \n";
-	traverseTreeQ(node, [](char n) { std::cout << n << " " ;});
+	std::cout << "Pre Order \n";
+	traverseTreePreO(node, [](char n) { std::cout << n << " " ;});
 	std::cout << "\n";	
 	
-	std::cout << "queue  recurse\n";
-	traverseTreeQRec(node, [](char n) { std::cout << n << " " ;});
+	std::cout << "Pre Order  recurse\n";	
+	traverseTreePreORec(node, [](char n) { std::cout << n << " " ;});
 	std::cout << "\n";
+	
+	std::cout << std::boolalpha;
+	std::cout << "is tree balanced: " << tBalaced(node) << "\n";	
 }
 
